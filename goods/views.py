816415a21d1f7c750ce6853d5c005c59f8bddf7b
yangchy <744421791@qq.com>
2017-07-10 11:48:32 +0800

    price_order

--- goods/views.py
+++ goods/views.py
@@ -6,9 +6,9 @@ from django.http import HttpResponse, JsonResponse
 from django.core.paginator import Paginator
 from django.forms.models import model_to_dict
 
-
 # Create your views here.
 goods_num_per_page = 1
+
 
 def index(request):
     context = {'title': '天天生鲜 - 首页', 'sectop': '0',
@@ -27,27 +27,33 @@ def goods_list(request, cat):
     category = get_object_or_404(GoodsCategory, pk=cat)
     new_list = category.goodsinfo_set.order_by('-id')[:2]
     goods = category.goodsinfo_set.order_by('-id')[:goods_num_per_page]
-    loop_num = (len(category.goodsinfo_set.order_by('-id'))-1)//goods_num_per_page + 1
+    loop_num = (len(category.goodsinfo_set.order_by('-id')) - 1) // goods_num_per_page + 1
     print(loop_num)
     # paginator = Paginator(goods, 4)
     # page = paginator.page(1)
     context = {'title': '天天生鲜 - 分类', 'sectop': '0',
                'user': get_object_or_404(TTSXUser, pk=request.session.get('login_id')), 'category': category,
-               'new_list': new_list, 'goods': goods, 'loop_num':loop_num}
+               'new_list': new_list, 'goods': goods, 'loop_num': loop_num}
     return render(request, 'goods/list.html', context=context)
 
 
 def goods_order(request):
     pn = int(request.GET.get('pn'))
     cat_id = request.GET.get('cat')
-    how = request.GET.get('how')
-    print(pn,'=====',how,'=====')
-    if how == 'price':
-        price_qur = get_object_or_404(GoodsCategory, pk=int(cat_id)).goodsinfo_set.order_by('-price')[goods_num_per_page*(pn-1):goods_num_per_page*pn]
+    how = str(request.GET.get('how'))
+    print(pn, '=====', how, '=====')
+    if how == '-1':
+        price_qur = get_object_or_404(GoodsCategory, pk=int(cat_id)).goodsinfo_set.order_by('-price')[
+                    goods_num_per_page * (pn - 1):goods_num_per_page * pn]
+    elif how == '1':
+        price_qur = get_object_or_404(GoodsCategory, pk=int(cat_id)).goodsinfo_set.order_by('price')[
+                    goods_num_per_page * (pn - 1):goods_num_per_page * pn]
     elif how == 'default':
-        price_qur = get_object_or_404(GoodsCategory, pk=int(cat_id)).goodsinfo_set.order_by('-id')[goods_num_per_page*(pn-1):goods_num_per_page*pn]
+        price_qur = get_object_or_404(GoodsCategory, pk=int(cat_id)).goodsinfo_set.order_by('-id')[
+                    goods_num_per_page * (pn - 1):goods_num_per_page * pn]
     else:
-        price_qur = get_object_or_404(GoodsCategory, pk=int(cat_id)).goodsinfo_set.order_by('-click')[goods_num_per_page*(pn - 1):goods_num_per_page*pn]
+        price_qur = get_object_or_404(GoodsCategory, pk=int(cat_id)).goodsinfo_set.order_by('-click')[
+                    goods_num_per_page * (pn - 1):goods_num_per_page * pn]
     price_list = []
     for price in price_qur:
         mdict = model_to_dict(price, fields=['id', 'name', 'price', 'pic', 'unit'])
@@ -59,9 +65,12 @@ def goods_order(request):
 
 def detail(request, gid):
     goods = get_object_or_404(GoodsInfo, pk=int(gid))
+    goods.click += 1
+    goods.save()
     new_list = goods.category.goodsinfo_set.order_by('-id')[:2]
     context = {'title': '天天生鲜 - 分类', 'sectop': '0',
-               'user': get_object_or_404(TTSXUser, pk=request.session.get('login_id')), 'goods':goods, 'new_list':new_list}
+               'user': get_object_or_404(TTSXUser, pk=request.session.get('login_id')),
+               'goods': goods, 'new_list': new_list}
     return render(request, 'goods/detail.html', context=context)
 
 
